1c760fc0e085a64d1758defb5a33e7fd1e1e95e4
Nick Pan <panick@panick-macbookpro3.roam.corp.google.com>
2020-04-18 14:22:51 -0400

    Expand main(), add insert_to_db() function.

--- import.py
+++ import.py
@@ -7,9 +7,10 @@ database: students.db (sqlite3)
 characters.csv schema: name, house, birth 
 Example row: Adelaide Murton,Slytherin,1982"""
 
-import csv
 
+import csv
 import getopt
+import sqlite3
 import sys
 
 
@@ -18,7 +19,7 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, "hi:",["input"])
     except getopt.GetoptError:
-        print("USAGE... import.py -i <import_file>")
+        print("ERROR... Correct usage: import.py -i <import_file>")
         sys.exit(2)
     # Define logic for -h and -i arguments
     for opt, arg in opts:
@@ -32,8 +33,37 @@ def main(argv):
     # Open characters.csv in read-only mode
     with open(import_file, newline='') as csvfile:
         char_reader = csv.reader(csvfile, delimiter=',')
+        # Skip first line
+        next(char_reader)
         for row in char_reader:
-            print(row)
+            full_name = row[0]
+            print(full_name)
+            full_name = full_name.split(' ')
+            print(full_name)
+            # Length of full name list is 2 meaning just full name, last name
+            if len(full_name) == 2:
+                first_name = full_name[0]
+                middle_name = ''
+                last_name = full_name[1]
+            else:
+                first_name = full_name[0]
+                middle_name = full_name[1]
+                last_name = full_name[2]   
+            house = row[1]
+            birth_year = row[2]
+            insert_to_db(first_name, middle_name, last_name, house, birth_year)
+
+# Helper function 
+def insert_to_db(first, middle, last, house, birth):
+    """ Takes in values to insert into SQLite3 database...
+    first: STRING : first name
+    middle: STRING : middle name
+    last: STRING : last name
+    house: STRING : Harry Potter house that character is a member of
+    birth: STRING : birthday of character"""
+    print("Inside insert_to_db!")
+    pass
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
